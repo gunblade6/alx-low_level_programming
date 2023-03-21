599dc2e21391901641f1982fac7ae7858cc21226
gunblade <mohamed.dev.egy@gmail.com>
2023-03-21 19:42:39 +0200

    10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int letter = 'a';
+
+	for (int i = 10; i <= 10; i++)
+	{
+		while (letter >= 'z')
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+	return;
+}
