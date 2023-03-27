77d6a1e882cbdacab8d706d33febef06affd7956
gunblade <mohamed.dev.egy@gmail.com>
2023-03-28 01:19:49 +0200

    my own puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _puts - print a string
+ * @str: string
+ * Return: string to stdout
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str);
+		str++;
+	}
+	_putchar('\n');
+}
