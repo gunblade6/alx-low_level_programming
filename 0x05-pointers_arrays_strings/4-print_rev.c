533efb429af945f6cf78874893aa2e54690501a2
gunblade <mohamed.dev.egy@gmail.com>
2023-03-28 01:30:00 +0200

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	for (len -= 1; len >= 0; len--)
+	{
+		_putchar(*s[len]);
+	}
+	_putchar('\n');
+}
