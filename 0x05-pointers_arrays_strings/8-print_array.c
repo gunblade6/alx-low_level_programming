c3300f11be020e5b3288b461feec994ea614b33d
gunblade <mohamed.dev.egy@gmail.com>
2023-03-29 01:53:59 +0200

    attemp 2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
+/**
+ *	print_array - print array
+ *	@a: array
+ *	@n: number variable
+ *	Return: n elements of array
+ *
+ */
 void print_array(int *a, int n)
 {
-	for (int i = 0; i < n; i++)
+	int i = 0;
+
+	while (i < n)
 	{
 		_putchar(a[i]);
+		i++;
 	}
 	_putchar('\n');
 }
