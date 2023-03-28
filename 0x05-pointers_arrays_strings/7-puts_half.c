4b5c5ffc6df3ed1ef6a21063da4951e138fbc5af
gunblade <mohamed.dev.egy@gmail.com>
2023-03-28 20:10:08 +0200

    attemp 1

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *	puts_half - half a string
+ *	@str: string
+ *	Return: half of a string
+ *
+ */
+void puts_half(char *str)
+{
+	len = _strlen(str);
+	printf("%d", len);
+}
