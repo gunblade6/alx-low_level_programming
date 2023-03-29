829874b623630d5c69976e8ff17d7b7b3d6bde6b
gunblade <mohamed.dev.egy@gmail.com>
2023-03-29 19:45:27 +0200

    update 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,29 +10,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int destLen = 0;
-	int srcLen = 0;
-	int i = 0;
+	int j;
 
-	while (dest[i] != '\0')
-	{
-		destLen++;
-		i++;
-	}
-	i = 0;
-
-	while (src[i] != '\0')
-	{
-		srcLen++;
-		i++;
-	}
-	i = 0;
-
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[j] = src[j];
+	for (; j < n; j++)
+		dest[j] = '\0';
 	return (dest);
 }
