08fcf354499dd4516060b82abc5ad4e29510136a
gunblade <mohamed.dev.egy@gmail.com>
2023-03-29 19:39:52 +0200

    update 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,30 +10,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int destLen = 0;
-	int srcLen = 0;
-	int i = 0;
+	int i = 0, j;
 
 	while (dest[i] != '\0')
-	{
-		destLen++;
 		i++;
-	}
-	i = 0;
-
-	while (src[i] != '\0')
-	{
-		srcLen++;
-		i++;
-	}
-	i = 0;
-
-	while (i < n)
-	{
-		dest[destLen] = src[i];
-		destLen++;
-		i++;
-	}
-	dest[destLen] = '\0';
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	dest[i] = '\0';
 	return (dest);
+}
 }
