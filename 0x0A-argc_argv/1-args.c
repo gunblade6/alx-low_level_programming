320c49512fe6dbad4821cbb015bbc2d64c7fe42a
gunblade <mohamed.dev.egy@gmail.com>
2023-04-08 01:32:34 +0200

    betty style

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ *	main - print the number of arguments
+ *	@argc: number of arguments
+ *	@argv: array of arguments
+ *	Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
