b3ad6ff43be32c8a2e28ca47b024b116de949663
gunblade <mohamed.dev.egy@gmail.com>
2023-04-08 01:29:03 +0200

    betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - print the name of the program
  * @argc: the number of arguments
  * @argv: array of arguments
- * Return: the name of the program
+ * Return: Always 0
  */
+
 int main(int argc, char *argv[])
-{	
+{
 	if (argc > 0)
 		printf("%s\n", argv[0]);
 	return (0);
