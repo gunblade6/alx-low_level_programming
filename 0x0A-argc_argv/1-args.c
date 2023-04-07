1b77c67c10f501e0372d3e3472fb2b9d6a719819
gunblade <mohamed.dev.egy@gmail.com>
2023-04-08 01:54:27 +0200

    new line

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,10 @@
  *	@argv: array of arguments
  *	Return: Always 0
  */
+
 int main(int argc, char __attribute__((unused)) *argv[])
 {
 	if (argc > 0)
-		printf("%d", argc - 1);
+		printf("%d\n", argc - 1);
 	return (0);
 }
