0df8f746f4200bff41d50d1a33cc9eef7e4d8c2f
gunblade <mohamed.dev.egy@gmail.com>
2023-04-08 03:13:12 +0200

    im getting nasty with this language man :)

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *	main - function
+ *	@argc: number of arguments
+ *	@argv: array of arguments
+ *	Return: 0 if all are number 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int total = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("%d\n", total);
+		return (0);
+	}
+	
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			total += atoi(argv[i]);
+		}
+	}
+
+	printf("%d\n", total);
+	return (0);
+}
