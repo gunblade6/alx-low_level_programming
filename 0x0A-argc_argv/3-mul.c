b66bbed7c8232cddfe2999886c6d5c9553236189
gunblade <mohamed.dev.egy@gmail.com>
2023-04-08 03:01:51 +0200

    if condition modification

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,14 @@ int main(int argc, char *argv[])
 {
 	int result;
 
-	if (argc == 3)
+	if (argc > 2)
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
 	}
 	else
 	{
+		printf("Error\n");
 		return (1);
 	}
 	return (0);
