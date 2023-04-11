077f9a7f1140e6a9fc436f524144aed033e423c3
gunblade <mohamed.dev.egy@gmail.com>
2023-04-11 17:28:03 +0200

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,13 +9,13 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	char arr[] = malloc(size * sizeof(char));
-	int i;
+	char **arr = malloc(size * sizeof(char));
+	unsigned int i;
 
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
 
-	return (arr);
+	return (*arr);
 }
