0f707b197a0902ad2496d958d65ebed80c3ceab2
gunblade <mohamed.dev.egy@gmail.com>
2023-04-11 21:44:53 +0200

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+char *_strdup(char *str)
+{
+	int i, length;
+	char *newStr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		length = strlen(str);
+
+		newStr = malloc(length * sizeof(char));
+		if (newStr == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; i < length; i++)
+		{
+			newStr[i] = str[i];
+		}
+	}
+
+	return (str);
+}
