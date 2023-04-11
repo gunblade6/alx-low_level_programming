17425863775e6e953af5c4ac4b553ffe6b583cce
gunblade <mohamed.dev.egy@gmail.com>
2023-04-11 17:32:43 +0200

    moved variables to top of function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,13 +4,13 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr = malloc(size * sizeof(char));
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
-	char *arr = malloc(size * sizeof(char));
-	unsigned int i;
 
 	for (i = 0; i < size; i++)
 	{
