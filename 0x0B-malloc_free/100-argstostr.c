8146ba4475c068d915f2f4750efdff4f4f0acbd8
gunblade <mohamed.dev.egy@gmail.com>
2023-04-12 01:04:56 +0200

    concat arguments'

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *	argstostr - concat all arguments
+ *	@ac: number or arguments
+ *	@av: array of arguments
+ */
+char *argstostr(int ac, char **av)
+{
+	int resultLen = 0;
+	int i, j;
+	int k = 0;
+	char *RESULT;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			resultLen++;
+
+		resultLen++;
+	}
+
+	RESULT = malloc(resultLen * sizeof(char));
+	if (RESULT == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			RESULT[k] = av[i][j];
+			k++;
+		}
+		RESULT[k] = '\n';
+		k++;
+	}
+
+	RESULT[k] = '\0';
+	return (RESULT);
+}
