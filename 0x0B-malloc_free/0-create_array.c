819724cc203ecc759fbdfaad22269fab04527c90
gunblade <mohamed.dev.egy@gmail.com>
2023-04-11 17:20:03 +0200

    float like a butterfyl ;D

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *arr = malloc(size * sizeof(char));
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+}
