00087b70be68c897c4742c35c6fbe95b29df1fad
gunblade <mohamed.dev.egy@gmail.com>
2023-04-12 01:20:51 +0200

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,47 +10,44 @@
  */
 char **strtow(char *str)
 {
-        int i, j, k, count = 0, len = 0;
-        char **arr;
+	int i, j, k, count = 0, len = 0;
+	char **arr;
 
-        if (str == NULL || *str == '\0')
-                return (NULL);
+	if (str == NULL || *str == '\0')
+		return (NULL);
 
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-                        count++;
-        }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
+	}
 
-        arr = malloc((count + 1) * sizeof(char *));
-        if (arr == NULL)
-                return (NULL);
+	arr = malloc((count + 1) * sizeof(char *));
+	if (arr == NULL)
+		return (NULL);
 
-        for (i = 0; i < count; i++)
-        {
-                while (*str == ' ')
-                        str++;
+	for (i = 0; i < count; i++)
+	{
+		while (*str == ' ')
+			str++;
 
-                len = 0;
-                while (str[len] != ' ' && str[len] != '\0')
-                        len++;
+		len = 0;
+		while (str[len] != ' ' && str[len] != '\0')
+			len++;
+		arr[i] = malloc((len + 1) * sizeof(char));
+		if (arr[i] == NULL)
+		{
+			for (k = 0; k < i; k++)
+				free(arr[k]);
 
-                arr[i] = malloc((len + 1) * sizeof(char));
-                if (arr[i] == NULL)
-                {
-                        for (k = 0; k < i; k++)
-                                free(arr[k]);
+			free(arr);
+			return (NULL);
+		}
+		for (j = 0; j < len; j++)
+			arr[i][j] = *str++;
 
-                        free(arr);
-                        return (NULL);
-                }
-
-                for (j = 0; j < len; j++)
-                        arr[i][j] = *str++;
-
-                arr[i][j] = '\0';
-        }
-
-        arr[i] = NULL;
-        return (arr);
+		arr[i][j] = '\0';
+	}
+	arr[i] = NULL;
+	return (arr);
 }
