220fe57866af4b55bbf9ec21cfe70013337a2f53
gunblade <mohamed.dev.egy@gmail.com>
2023-04-14 00:19:27 +0200

    typo in variable name

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	str = malloc(nmemb * size);
+	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
 
