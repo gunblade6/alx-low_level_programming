1053320b7b448084a16d19d68b6160a6035dadfe
gunblade <mohamed.dev.egy@gmail.com>
2023-04-14 00:31:40 +0200

    added if condition if args = 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,9 +14,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int s;
 	char *ptr;
 
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
 	s = nmemb * size;
+
 	if ((ptr = malloc(s)) == NULL)
 		return (NULL);
+
 	memset(ptr, 0, s);
+
 	return (ptr);
 }
