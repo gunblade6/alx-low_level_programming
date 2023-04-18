c35ea212060797ead000d9a1b9231a94884064b4
gunblade <mohamed.dev.egy@gmail.com>
2023-04-18 17:57:47 +0200

    trying to use . sign instead of ->

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,24 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
 
 /**
- * init_dog - dog structure
- * @d: the struct
- * @name: dog's name
- * @age: dog's age
- * @owner: the owner of dog
- */
-
+ * init_dog - initialize dog struct
+ *
+ * @d: input pointer to struct dog
+ * @name: input for dog name
+ * @age: input for dog age
+ * @owner: input for dog owner
+ *
+ * Return: nothing
+*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-	;
-	else
+	if (d)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		/*d->name = name;*/
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 	}
 }
