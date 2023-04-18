b03cb9cd3ce595350e15f16611906fe82b109922
gunblade <mohamed.dev.egy@gmail.com>
2023-04-18 05:00:32 +0200

    print my name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#ifndef FILE_H
+#define FILE_H
+#include <stdio.h>
+
+/*
+ *	main - print my name
+ *	Return: always 0
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
+
+#endif
+
