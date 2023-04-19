feb1e5f175273617a9ad386d5f97499b1fb1e1bb
gunblade <mohamed.dev.egy@gmail.com>
2023-04-19 22:02:43 +0200

    wrote for loop normally

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -13,5 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int i;
 
 	for (i = 0; i < size; i++)
+	{
 		action(array[i]);
+	}
 }
