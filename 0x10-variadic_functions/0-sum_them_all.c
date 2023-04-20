ff722bc1cfe78dfd668b24d25c07e78a7e8c4514
gunblade <mohamed.dev.egy@gmail.com>
2023-04-20 23:50:55 +0200

    changed the condition place

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-
-	if (n == 0)
-		return (0);
-
 	unsigned int i;
 	int sum = 0;
 
-
 	va_list args;
 
+	if (n == 0)
+		return (0);
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
