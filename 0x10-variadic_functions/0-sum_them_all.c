64da7a51d10a1f8520e29b3f8f474a3e93c05b52
gunblade <mohamed.dev.egy@gmail.com>
2023-04-20 23:44:15 +0200

    enhancement

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,19 +3,16 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	int sum = 0;
+
 	va_list args;
 
 	va_start(args, n);
-
-	int i, sum = 0;
-
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(args, int);
-	}
 
 	va_end(args);
-
 	return (sum);
 }
 
