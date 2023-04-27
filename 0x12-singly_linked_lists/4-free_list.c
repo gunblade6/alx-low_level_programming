9b0714d83365b90b253c57a482c14df4000dc7ae
gunblade <mohamed.dev.egy@gmail.com>
2023-04-27 23:42:16 +0200

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,61 +2,21 @@
 #include "lists.h"
 
 /**
- * add_node - function with two arguments
- * @head: double pointer to linked list
- * @str: string pointer
+ * free_list - function with one argument
+ * @head: pointer to list_t
  *
- * Description: adds a new node at the beginning
- * Return: address of new element
+ * Description: frees a list
+ * Return: na
  */
-list_t *add_node(list_t **head, const char *str)
+void free_list(list_t *head)
 {
-	int count = 0;
-	list_t *temp;
+	list_t *cursor;
 
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
-		return (NULL);
-	temp->str = _strdup(str);
-	while (str[count] != '\0')
-		count++;
-	temp->len = count;
-	temp->next = *head;
-	*head = temp;
-	return (temp);
-}
-
-/**
- * *_strdup - function with one argument
- * @str: string argument
- *
- * Description: returns a pointer to allocated space in memory
- * Return: pointer
- */
-char *_strdup(const char *str)
-{
-	int i, j;
-	char *ptr;
-
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (*(str + i) != '\0')
+	while (head)
 	{
-		i++;
+		cursor = head->next;
+		free(head->str);
+		free(head);
+		head = cursor;
 	}
-
-	ptr = malloc(sizeof(char) * i + 1);
-
-	if (ptr == NULL)
-		return (NULL);
-
-	j = 0;
-	while (str[j] != '\0')
-	{
-		ptr[j] = str[j];
-		j++;
-	}
-	ptr[j] = '\0';
-	return (ptr);
 }
